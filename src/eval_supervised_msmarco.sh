79d0734bd92ccd5aa7e6214aad25f2bd5f37d97e
hzg0601 <hzg0601@163.com>
2023-11-30 15:40:09 +0800

    update eval_msmarco.sh

--- src/eval_supervised_msmarco.sh
+++ src/eval_supervised_msmarco.sh
@@ -9,7 +9,7 @@ python -m src/eval_msmarco \
 --encoder $current_dir/model_output/supervised_train \
 --fp16 \
 --add_instruction \
---k 100 >eval_msmarco.log 2>&1 
+--k 100 >finetuned_msmarco.log 2>&1 
 
 # eval finetuned model on private data 
 python -m src/eval_msmarco \
@@ -17,7 +17,7 @@ python -m src/eval_msmarco \
 --fp16 \
 --add_instruction \
 -- private_data_path $current_dir/data/supervised_finetune_data.jsonl
---k 100 >eval_private.log 2>&1
+--k 100 >finetuned_private.log 2>&1
 
 # eval finetuned model on private data, but add some public data as candidate corpus 
 python -m src/eval_msmarco \
@@ -25,7 +25,7 @@ python -m src/eval_msmarco \
 --fp16 \
 --add_instruction \
 -- private_data_path $current_dir/data/supervised_finetune_data.jsonl
---k 100 >eval_private.log 2>&1 
+--k 100 >finetuned_private_extra_corpus.log 2>&1 
 
 # -------------------------------------------------------
 # eval base model on public data 
@@ -33,7 +33,7 @@ python -m src/eval_msmarco \
 --encoder /home/star/models/bge-large-zh-v1.5 \
 --fp16 \
 --add_instruction \
---k 100 >eval_msmarco.log 2>&1 
+--k 100 >base_msmarco.log 2>&1 
 
 # eval base model on private data 
 python -m src/eval_msmarco \
@@ -41,7 +41,7 @@ python -m src/eval_msmarco \
 --fp16 \
 --add_instruction \
 -- private_data_path $current_dir/data/supervised_finetune_data.jsonl
---k 100 >eval_private.log 2>&1
+--k 100 >base_private.log 2>&1
 
 # eval base model on private data, but add some public data as candidate corpus 
 python -m src/eval_msmarco \
@@ -50,4 +50,4 @@ python -m src/eval_msmarco \
 --add_instruction \
 --private_data_path $current_dir/data/supervised_finetune_data.jsonl
 --add_extra_corpus True
---k 100 >eval_private.log 2>&1 
+--k 100 >base_private_extra_corpus.log 2>&1 
